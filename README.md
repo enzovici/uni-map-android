ec0d12357d5b34f79924984f74068294dbb823df
Enzo Vici <Enzo Vici@DESKTOP-S7B9F9U.Home>
2016-03-21 21:59:13 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # uni-map-android
 Interactive Map of UNISA campus built with Google Mas API V3 - Android
+
+Dal 1988 l'Università di Salerno ha sede nella Valle dell'Irno, a pochi chilometri da Salerno e da Avellino, allo snodo di incroci autostradali che la rendono centrale e facilmente raggiungibile.
+Il tratto distintivo dell'Ateneo di Salerno è la sua configurazione in forma di campus: quello principale di Fisciano è situato in via Giovanni Paolo II e ha una superficie di 1.200.000 mq, mentre quello di Baronissi, situato in via S. Allende e con una superficie di 150.000 mq, ospita esclusivamente il dipartimento di Medicina e Chirurgia. 
+Il campus dell’Università degli studi di Salerno offre diversi corsi di studi e servizi come: biblioteche, impianti sportivi, residenze, mensa, la cappella e altro.
+Il campus riveste un’ampia superficie e i dipartimenti dell’università risiedono in vari edifici del campus. L’orientamento all’interno del Campus è complicato a causa della somiglianza tra i vari edifici e per chi si trova alle prime esperienze della vita nel campus può sentirsi spaesato.
+Inoltre non esistono mappe che indicano in modo adeguato gli edifici e i percorsi per raggiungerli.
+L’Ateneo negli ultimi anni, ha accolto e accompagnato nel percorso di studi un numero sempre crescente di studenti con disabilità. Inoltre ha dedicato una serie di servizi per gli studenti disabili. Muoversi e orientarsi nel campus può risultare difficoltoso soprattutto non conoscendo i percorsi alternativi.
+
+L’applicazione Uni Map nasce principalmente con lo scopo di facilitare l’orientamento geografico di studenti e persone diversamente abili all’interno del campus di Fisciano, al fine di garantire un minimo di supporto e autonomia. 
+L’applicazione permetterà, grazie ad una visione dall’alto della mappa del campus di visualizzare la posizione dell’utente e permetterà di mostrare i percorsi decidendo un punto di partenza e un punto di arrivo. Questi percorsi evitano barriere architettoniche, scale o altre complicazioni relative ad una disabilità motoria. 
+Inoltre l’applicazione permetterà un orientamento interno agli edifici del dipartimento di Informatica in modo da guidare l’utente anche all’interno dell’edificio.
+Uni Map offre anche la possibilità di visualizzare sulla mappa i punti di maggiore interesse e offrire informazioni generali sulle varie facoltà (Orari, Indirizzo, Fax, Direttore).
---
+++ assets/www/cordova_plugins.js
@@ -0,0 +1,112 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/de.appplant.cordova.plugin.email-composer/www/email_composer.js",
+        "id": "de.appplant.cordova.plugin.email-composer.EmailComposer",
+        "pluginId": "de.appplant.cordova.plugin.email-composer",
+        "clobbers": [
+            "cordova.plugins.email",
+            "plugin.email"
+        ]
+    },
+    {
+        "file": "plugins/com.developersanddesigners.PhonegapDialogs/www/notification.js",
+        "id": "com.developersanddesigners.PhonegapDialogs.notification",
+        "pluginId": "com.developersanddesigners.PhonegapDialogs",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/com.developersanddesigners.PhonegapDialogs/www/android/notification.js",
+        "id": "com.developersanddesigners.PhonegapDialogs.notification_android",
+        "pluginId": "com.developersanddesigners.PhonegapDialogs",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/ionic-plugin-keyboard/www/android/keyboard.js",
+        "id": "ionic-plugin-keyboard.keyboard",
+        "pluginId": "ionic-plugin-keyboard",
+        "clobbers": [
+            "cordova.plugins.Keyboard"
+        ],
+        "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-tts/www/tts.js",
+        "id": "cordova-plugin-tts.tts",
+        "pluginId": "cordova-plugin-tts",
+        "clobbers": [
+            "TTS"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "id": "cordova-plugin-whitelist.whitelist",
+        "pluginId": "cordova-plugin-whitelist",
+        "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "id": "cordova-plugin-device.device",
+        "pluginId": "cordova-plugin-device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-geolocation/www/android/geolocation.js",
+        "id": "cordova-plugin-geolocation.geolocation",
+        "pluginId": "cordova-plugin-geolocation",
+        "clobbers": [
+            "navigator.geolocation"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-geolocation/www/PositionError.js",
+        "id": "cordova-plugin-geolocation.PositionError",
+        "pluginId": "cordova-plugin-geolocation",
+        "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-globalization/www/GlobalizationError.js",
+        "id": "cordova-plugin-globalization.GlobalizationError",
+        "pluginId": "cordova-plugin-globalization",
+        "clobbers": [
+            "window.GlobalizationError"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-globalization/www/globalization.js",
+        "id": "cordova-plugin-globalization.globalization",
+        "pluginId": "cordova-plugin-globalization",
+        "clobbers": [
+            "navigator.globalization"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+        "id": "cordova-plugin-splashscreen.SplashScreen",
+        "pluginId": "cordova-plugin-splashscreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "de.appplant.cordova.plugin.email-composer": "0.8.2",
+    "com.developersanddesigners.PhonegapDialogs": "1.1.1",
+    "ionic-plugin-keyboard": "1.0.8",
+    "cordova-plugin-tts": "0.2.3",
+    "cordova-plugin-whitelist": "1.2.1",
+    "cordova-plugin-device": "1.1.1",
+    "cordova-plugin-geolocation": "2.1.0",
+    "cordova-plugin-globalization": "1.0.3",
+    "cordova-plugin-splashscreen": "3.2.1"
+}
+// BOTTOM OF METADATA
+});
